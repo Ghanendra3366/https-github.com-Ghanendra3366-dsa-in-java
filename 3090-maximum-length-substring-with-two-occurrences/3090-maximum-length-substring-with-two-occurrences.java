d24df88e8a70aa64b91d7fa69912b9f0364d9411
Ghanendra tiwari <179313226+Ghanendra3366@users.noreply.github.com>
2026-08-14 11:23:59 +0530

    Time: 2 ms (54.5%), Space: 45 MB (17.84%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -0,0 +1,20 @@
+
+class Solution {
+
+    public int maximumLengthSubstring(String s) {
+        int n = s.length();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                int ch = s.charAt(j) - 'a';
+                freq[ch]++;
+                if (freq[ch] > 2) {
+                    break;
+                }
+                res = Math.max(res, j-i +1);
+            }
+        }
+        return res;
+    }
+}
