011228c395d5f46732c646cd18d8d4a4f10649d9
Ghanendra tiwari <179313226+Ghanendra3366@users.noreply.github.com>
2026-08-20 22:30:32 +0530

    Time: 9 ms (14.76%), Space: 46.6 MB (23.84%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Arrays.sort(nums);
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]!=nums[i+1]){
+                return false;
+            }
+            i++;
+            i++;
+        }
+        return true;
+    }
+}
