28c70c29d31f88ec3eff76df940ff5d7eb84c8bf
Ghanendra tiwari <179313226+Ghanendra3366@users.noreply.github.com>
2026-09-16 19:16:12 +0530

    Time: 26 ms (75.48%), Space: 42.1 MB (89.03%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,28 +1,23 @@
 class Solution {
+
+    private static final int MOD = 1000000007;
+
     public int numberOfSets(int n, int k) {
-        long MOD = 1_000_000_007;
-        long res = combination(n + k - 1, 2 * k, MOD);
-        return (int) res;
-    }
-    private long combination(int n, int k, long mod) {
-        if (k < 0 || k > n) return 0;
-        if (k == 0 || k == n) return 1;
-        if (k > n / k) k = n - k; 
-        long num = 1, den = 1;
-        for (int i = 0; i < k; i++) {
-            num = (num * (n - i)) % mod;
-            den = (den * (i + 1)) % mod;
+        int[] dp = new int[n];
+        int[] prefixSums = new int[n + 1];
+        for (int j = 0; j < n; j++) {
+            dp[j] = 1;
+            prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
         }
-        return (num * power(den, mod - 2, mod)) % mod;
-    }
-    private long power(long base, long exp, long mod) {
-        long res = 1;
-        base %= mod;
-        while (exp > 0) {
-            if ((exp & 1) == 1) res = (res * base) % mod;
-            base = (base * base) % mod;
-            exp >>= 1;
+        for (int i = 1; i <= k; i++) {
+            dp[0] = 0;
+            for (int j = 1; j < n; j++) {
+                dp[j] = (dp[j - 1] + prefixSums[j]) % MOD;
+            }
+            for (int j = 0; j < n; j++) {
+                prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+            }
         }
-        return res;
+        return dp[n - 1];
     }
 }
